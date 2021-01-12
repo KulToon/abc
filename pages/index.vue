111f6a7e93f870c6bf59532da88d999f87018140
KulToon <bankbugsbunny@gmail.com>
2021-01-11 18:53:16 -0800

    try

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,26 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        sec
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    aaaa
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+   data() {
+    return {
+
+    }
+  },
+  asyncData({ app, store }) {
+    const url = 'http://localhost:3000/api/h';
+    return app.$axios.get(url).then(res => {
+      console.log(res.data);
+      //store.commit("productRegister", res.data.data)
+    })
+  }
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
