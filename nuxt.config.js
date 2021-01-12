78b693cc789c4a8f2c5dbf4c49cf33e3a47a0741
KulToon <bankbugsbunny@gmail.com>
2021-01-12 08:44:37 -0800

    seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'sec',
+    title: 'Vending Machine',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'VendingMachine, drinks, snack', content: 'th' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
