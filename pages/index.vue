d3999449f96f2cbe9c7e0c71db0f0a5544306fdb
KulToon <bankbugsbunny@gmail.com>
2021-01-11 23:14:42 -0800

    test3

--- pages/index.vue
+++ pages/index.vue
@@ -23,13 +23,13 @@ export default {
 
     }
   },
-  // asyncData({ app, store }) {
-  //   const url = 'http://localhost:3000/api/h';
-  //   return app.$axios.get(url).then(res => {
-  //     //console.log(res.data);
-  //     store.commit("productRegister", res.data.data)
-  //   })
-  // }
+  asyncData({ app, store }) {
+    const url = 'http://localhost:3000/api/h';
+    return app.$axios.get(url).then(res => {
+      //console.log(res.data);
+      store.commit("productRegister", res.data.data)
+    })
+  }
 }
 </script>
 
