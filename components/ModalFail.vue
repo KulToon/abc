9fc4753e5a61690724552f02dbb6edb99ba7c78d
KulToon <bankbugsbunny@gmail.com>
2021-01-12 08:26:04 -0800

    image

--- components/ModalFail.vue
+++ components/ModalFail.vue
@@ -2,7 +2,7 @@
     <div class="d-flex flex-colunm bg-danger/ align-items-cente/r"> 
         <h1>Not enough coin for this item</h1>
         <img 
-            src="https://www.flaticon.com/svg/vstatic/svg/190/190108.svg?token=exp=1610440143~hmac=31111b2a1c5e07d01c56180609b91a5f"
+            src="~/assets/piggy-bank.svg"
         
         />
     </div>
